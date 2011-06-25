000227b525946868432e76424385e9be6c118a42
Reed La Botz <reedlabotz@gmail.com>
2011-06-24 21:30:00 -0500

    some design

--- web/script/boggle.js
+++ web/script/boggle.js
@@ -54,47 +54,54 @@ function boggle_init() {
   }
   
   board_div.append(table);
-
+  resizeScreen();
   shake();
 }
 
 function shake() {
-  reset();
-  var temp_cubes = new Array();
-  var i, j;
-  var board_id = "";
-  
-  for(i=0; i< board.width*board.height; i++)
-    temp_cubes.push(i);
-  
-  for(i=0; i<board.height; i++) {
-    for (j=0; j<board.width; j++) {
-      var index = i + "-" + j;
-      var random = Math.floor(Math.random() * temp_cubes.length);
-      var cube = temp_cubes.splice(random,1)
-      var letter =  cubes[cube][Math.floor(Math.random()*6)];
-      if (letter == "Qu") {
-        board_id += "q"
-        document.getElementById(index).innerHTML = "<span class=\"qu\">Qu</span>";
+   reset();
+   for(i=0; i<board.height; i++) {
+      for (j=0; j<board.width; j++) {
+         $("#"+ i + "-" + j).html("<img src='loader.gif' />");
       }
-      else {
-        board_id += letter.toLowerCase();
-        document.getElementById(index).innerHTML = letter;
+   }
+   var temp_cubes = new Array();
+   var i, j;
+   var board_id = "";
+  
+   for(i=0; i< board.width*board.height; i++)
+      temp_cubes.push(i);
+  
+   for(i=0; i<board.height; i++) {
+      for (j=0; j<board.width; j++) {
+         var index = i + "-" + j;
+         var random = Math.floor(Math.random() * temp_cubes.length);
+         var cube = temp_cubes.splice(random,1);
+         var letter =  cubes[cube][Math.floor(Math.random()*6)];
+         if (letter == "Qu")
+            board_id += "q";
+         else
+            board_id += letter.toLowerCase();
       }
-    }
-  }
-  $.get("http://localhost:3000/?letters="+board_id,function(data){
-       results = $.parseJSON(data);
-       updateStats();
-       $("#wordEntry").removeAttr("disabled");
-       $("#wordEntry").focus();
-       startTime = new Date().getTime();
-       updateTimer();
-       timeout = setTimeout("endGame()",180500);
-       interval = setInterval("updateTimer()",1000);
-    });
-    
-    
+   }
+   $.get("http://localhost:3000/?letters="+board_id,function(data){
+      results = $.parseJSON(data);
+      for(i=0; i<board.height; i++) {
+         for (j=0; j<board.width; j++) {
+            var letter = board_id.charAt(i+4*j).toUpperCase()
+            if(letter == "Q")
+               $("#"+i+"-"+j).html("<span class=\"qu\">Qu</span>");
+            else
+               $("#"+i+"-"+j).html(letter);
+         }
+      }
+      $("#wordEntry").removeAttr("disabled");
+      $("#wordEntry").focus();
+      startTime = new Date().getTime();
+      timeout = setTimeout("endGame()",180500);
+      interval = setInterval("updateTimer()",1000);
+      $("#totalCount").html(results.Count)
+   });
 }
 
 function checkWord(){
@@ -127,6 +134,8 @@ function placeWord(word,missed){
    newWord.attr('id',"word-"+word)
    if(missed)
       newWord.addClass("missed-word");
+   else
+      checkScrollbar();
    newWord.html(word);
    lastId = ($.inArray(word,correctWords)) - 1
    if(lastId < 0){
@@ -136,14 +145,24 @@ function placeWord(word,missed){
 }
 
 function updateStats(){
-   $("#stats").html("<p>Guesses: "+guesses+"</p><p>Correct: "+correct+"/"+results.Count+"</p><p>Score: "+score+"</p><p>Max Score: "+results.MaxScore+"</p>");
+   $("#correctCount").html(correct);
+   $("#score").html(score);
 }
 
 function reset(){
+   $("#shake").hide();
+   $("#endGame").show();
    $("#wordList").html("");
-   $("#stats").html("");
-   $("#timer").html("");
+   $("#timer").html("3:00");
+   $("#correctCount").html("0");
+   $("#score").html("0");
+   $("#totalCount").html("0")
    $("#wordEntry").val("");
+   for(i=0; i<board.height; i++) {
+      for (j=0; j<board.width; j++) {
+         $("#"+ i + "-" + j).html("");
+      }
+   }
    correct = 0;
    guesses = 0;
    score = 0;
@@ -162,7 +181,7 @@ function updateTimer(){
    if(sec<10){
       secSpace = "0";
    }
-   $("#timer").html("<p>Time: "+min+":"+secSpace+sec+"</p>");
+   $("#timer").html(min+":"+secSpace+sec);
    
    if(seconds == 30)
       $("#timer").effect("highlight", {"color":"#E77471"}, 5000);
@@ -204,5 +223,33 @@ function endGame(){
             $("#word-"+word).effect("highlight", {"color":"#FFF8C6"}, 5000);
          }
       });
+      checkScrollbar();
    });
+   $("#shake").show();
+   $("#endGame").hide();
+}
+
+function resizeScreen(){
+   var width = $(window).width();
+   var height = $(window).height();
+   $("#rightColumn").width(width-238-65);
+   $("#wordList").width(width-238-65-20);
+   $("#wordList").height(height-100);
+   
+   wordListWidth = $("#wordList").width();
+   columns = "" + Math.floor(wordListWidth/85);
+   if(columns < 2)
+      columns = 2;
+   $("#wordList").css("-webkit-column-count",columns);
+   checkScrollbar();
+}
+
+function checkScrollbar(){
+   var before = $("#wordList").scrollLeft();
+   $("#wordList").scrollLeft(85);
+   if($("#wordList").scrollLeft()<10)
+      $("#wordList").css("overflow-x","hidden");
+   else
+      $("#wordList").css("overflow-x","scroll");
+   $("#wordList").scrollLeft(before);
 }
